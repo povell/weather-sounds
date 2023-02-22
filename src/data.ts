196adc2db3443e004699ea2daf9221a4306814fd
Pavel Ivshin <povelivshin@gmail.com>
2023-02-22 23:23:15 +0300

    fixed types

--- src/data.ts
+++ src/data.ts
@@ -1,26 +1,34 @@
 import { IButtonData } from './models';
+import sunIcon from './assets/icons/sun.svg';
+import rainIcon from './assets/icons/cloud-rain.svg';
+import winterIcon from './assets/icons/cloud-snow.svg';
+import summerBg from './assets/summer-bg.jpg';
+import rainyBg from './assets/rainy-bg.jpg';
+import winterBg from './assets/winter-bg.jpg';
+import summerSound from './assets/sounds/summer.mp3';
+import rainSound from './assets/sounds/rain.mp3';
+import winterSound from './assets/sounds/winter.mp3';
+
 export const buttons: IButtonData[] = [
   {
     id: 1,
-    icon: './icons/sun.svg',
-    backgroundImg: './summer-bg.jpg',
-    soundFile: './sounds/summer.mp3',
+    icon: sunIcon,
+    backgroundImg: summerBg,
+    soundFile: summerSound,
     isSelected: false,
   },
   {
     id: 2,
-    icon: './icons/cloud-rain.svg',
-    backgroundImg: './rainy-bg.jpg',
-    soundFile: './sounds/rain.mp3',
+    icon: rainIcon,
+    backgroundImg: rainyBg,
+    soundFile: rainSound,
     isSelected: false,
   },
   {
     id: 3,
-    icon: './icons/cloud-snow.svg',
-    backgroundImg: './winter-bg.jpg',
-    soundFile: './sounds/winter.mp3',
+    icon: winterIcon,
+    backgroundImg: winterBg,
+    soundFile: winterSound,
     isSelected: false,
   },
 ];
-
-export const pauseIcon: string = './icons/pause.svg';
